a7e09ad87337699b73d1502903de27c3e56caa63
indiana <indimar.w1@gmail.com>
2023-10-23 17:59:03 +0200

    sibida10

---
+++ Repaso1/ProyectoAlgebra/triangles.ts
@@ -0,0 +1,14 @@
+import { Point } from "./point";
+
+export class Triangle{
+    private vertx1: Point;
+    private vertx2: Point;
+    private vertx3: Point;
+
+    constructor(vertx1: Point, vertx2: Point, vertx3: Point){
+        this.vertx1 = vertx1;
+        this.vertx1 = vertx2;
+        this.vertx3 = vertx3;
+    }
+public calculateLengthSides()
+}
