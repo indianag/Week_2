019ab6291d8b3ee6e45774ba926da1de4edd0524
indiana <indimar.w1@gmail.com>
2023-10-24 10:24:15 +0200

    update

---
+++ Repaso1/ProyectoAlgebra/triangleTest.ts
@@ -0,0 +1,12 @@
+import { Triangle } from "./triangles";
+import { Point } from "./point";
+
+
+
+let myTriangle = new Triangle(1,2);
+
+
+
+
+
+
--- Repaso1/ProyectoAlgebra/triangles.ts
+++ Repaso1/ProyectoAlgebra/triangles.ts
@@ -10,5 +10,34 @@ export class Triangle{
         this.vertx1 = vertx2;
         this.vertx3 = vertx3;
     }
-public calculateLengthSides()
+    public setVertx1(newVertx1: Point): void {
+        this.vertx1 = newVertx1
+    }
+    public getVertx1(): Point {
+        return this.vertx1
+    }
+    public setVertx2(newVertx2: Point): void {
+        this.vertx2 = newVertx2
+    }
+    public getVertx2(): Point {
+        return this.vertx2
+    }
+    public setVertx3(newVertx3: Point): void {
+        this.vertx3 = newVertx3
+    }
+    public getVertx3(): Point {
+        return this.vertx3
+    }
+    public calculateLengthSides():number[]{
+        let puntoAB:number = calculateDistance(this.vertx1);
+        let puntoBC:number = calculateDistance(this.vertx2);
+        let puntoCA:number = calculateDistance(this.vertx3);
+        const posiciones = [puntoAB,puntoBC,puntoCA];
+    }
 }
+
+
+
+
+// const distancia = Math.sqrt(((this.x - anotherPoint.x) ** 2) + ((this.y - anotherPoint.y) ** 2));
+// return distancia;
